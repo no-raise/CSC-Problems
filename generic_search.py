bbbfdd576b9f2938fcc57f565c1a9ab2b94da5a8
Nouraiz Imran <nouraiz_imran@hotmail.com>
2019-10-31 16:16:02 -0400

    Added generic search and started maze.py

---
+++ generic_search.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+from typing import TypeVar, Iterable, Sequence, List, Callable, Set, Deque, Dict, Any, Optional
+from typing_extensions import Protocol
+from heapq import heappush, heappop
+
+T = TypeVar('T')
+
+def linear_contains(iterable: Iterable[T], key: T) -> bool:
+    for item in iterable:
+        if item == key:
+            return True
+    return False
+
+C = TypeVar('C', bound="Comparable")
+
+class Comparable(Protocol):
+    def __eq__(self, other: Any) -> bool:
+        ...
+    def __lt__(self: C, other: C) -> bool:
+        ...
+    def __gt__(self: c, other: C) -> bool:
+        return (not self < other) and self != other
+    def __le__(self: c, other: C) -> bool:
+        return self < other or self == other
+    def __ge__(self: c, other: C) -> bool:
+        return not self < other
+
+def binary_contains(sequence: Sequence[C], key: C) -> bool:
+    low: int = 0
+    high: int = len(sequence) - 1
+    while low <= high:
+        mid: int = (low + high) // 2
+        if sequence[mid] < key:
+            low = mid + 1
+        elif sequence[mid] > key:
+            high = mid - 1
+        else:
+            return True
+    return False
+
+if __name__ == "__main__":
+    print(linear_contains([1, 5, 15, 15, 15, 15, 20], 5))
