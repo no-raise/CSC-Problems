638a44968f3a2cf0054a5e6279d7aa9d2bd9d52f
Nouraiz Imran <nouraiz_imran@hotmail.com>
2019-11-26 20:52:14 -0500

    added main and implemented solution using bfs.

--- missionaries_and_cannibals/main.py
+++ missionaries_and_cannibals/main.py
@@ -1,2 +1,34 @@
-from state_repr import MAX_NUM, MC_state
+from sys import path
+from os.path import dirname as dir
+path.append(dir(path[0]) + '\\generic_searches')
 
+from state_repr import MAX_NUM, MC_state
+from generic_searches import bfs, node_to_path
+from generic_data_structures import Node
+from typing import List, Optional
+
+
+def display_solution(path: List[MC_state]) -> None:
+    if len(path) == 0:
+        return 
+    
+    old_state: MC_state = path[0]
+    print(old_state)
+    for current_state in path[1:]:
+        if current_state.boat:
+            print(f'{old_state.right_m - current_state.right_m} missionaries and {old_state.right_c - current_state.right_c} cannibals moved from left to right bank')
+        else:
+            print(f'{old_state.left_m - current_state.left_m} missionaries and {old_state.left_c - current_state.left_c} cannibals moved from right to left bank')
+        print(current_state)
+        old_state = current_state
+    
+if __name__ == "__main__":
+    start: MC_state = MC_state(MAX_NUM, MAX_NUM, True)
+    solution: Optional[Node[MC_state]] = bfs(start, MC_state.goal_test, MC_state.successors)
+    if solution is None:
+        print("No solution found.")
+    else:
+        path = node_to_path(solution)
+        display_solution(path)
+
+        
--- missionaries_and_cannibals/state_repr.py
+++ missionaries_and_cannibals/state_repr.py
@@ -1,7 +1,9 @@
-import sys
-sys.path.append("generic-searches/")
-
 from __future__ import annotations
+
+from sys import path
+from os.path import dirname as dir
+path.append(dir(path[0]) + '\\generic_searches')
+
 from typing import List, Optional
 from generic_searches import node_to_path, bfs
 from generic_data_structures import Node
@@ -17,12 +19,10 @@ class MC_state:
         self.boat: bool = boat
 
     def __str__(self) -> str:
-        return (f"""On the west bank there are {self.left_m} missionaries and {self.left_c}cannibals.\n
-                    On the east bank there are {self.right_m} missionaries and {self.right_c} cannibals.\n
-                    The boat is on the {'left' if self.boat else 'right'} bank.""")
+        return (f'On the west bank there are {self.left_m} missionaries and {self.left_c} cannibals.\nOn the east bank there are {self.right_m} missionaries and {self.right_c} cannibals.\nThe self.boat is on the {"left" if self.boat else "right"} bank.')
 
     def goal_test(self) -> bool:
-        return self.islegal and self.left_m == MAX_NUM and self.left_c == MAX_NUM
+        return self.islegal and self.right_m == MAX_NUM and self.right_c == MAX_NUM
 
     @property
     def islegal(self) -> bool:
@@ -36,17 +36,17 @@ class MC_state:
         result: List[MC_state] = []
         if self.boat:
             if self.left_m > 0:
-                result.append(MC_state(self.left_m - 1, self.left_c, not boat))
+                result.append(MC_state(self.left_m - 1, self.left_c, not self.boat))
             if self.left_c > 0:
-                result.append(MC_state(self.left_m, self.left_c - 1, not boat))
+                result.append(MC_state(self.left_m, self.left_c - 1, not self.boat))
             if self.left_c > 0 and self.left_m > 0:
-                result.append(MC_state(self.left_m - 1, self.left_c - 1, not boat))    
+                result.append(MC_state(self.left_m - 1, self.left_c - 1, not self.boat))    
         else:
             if self.right_m > 0:
-                result.append(MC_state(self.left_m + 1, self.left_c, not boat))
+                result.append(MC_state(self.left_m + 1, self.left_c, not self.boat))
             if self.right_c > 0:
-                result.append(MC_state(self.left_m, self.left_c + 1, not boat))
+                result.append(MC_state(self.left_m, self.left_c + 1, not self.boat))
             if self.right_m > 0 and self.right_c > 0:
-                result.append(MC_state(self.left_m + 1, self.left_c + 1, not boat))
+                result.append(MC_state(self.left_m + 1, self.left_c + 1, not self.boat))
         return [x for x in result if x.islegal]
         
